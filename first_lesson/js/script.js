a1b3c15a7bc44ca1be177dbff9425448e2469718
Konstantin <Konstantaond@gmail.com>
2019-01-28 20:00:34 +0300

    это новый коммит

--- first_lesson/js/script.js
+++ first_lesson/js/script.js
@@ -1,4 +1,22 @@
-'use strict'
+'use strict';
 
 let money = prompt("Ваш бюджет на месяц", "1400$");
-console.log(money);
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appData = {
+    budget: money,
+    timeData : time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false 
+}
+
+let exps1, exps2;
+exps1 = prompt('Введите обязательную статью расходов в этом месяце','');
+appData.expenses[exps1] = prompt('Во сколько обойдется?', '');
+//appData - главный обьект
+//.expenses обьект в обьекте
+//[exp1] обращение к переменной
+
+alert('Вам понадобится' + appData.expenses[exps1]/30 +'рублей в день для exps1');
